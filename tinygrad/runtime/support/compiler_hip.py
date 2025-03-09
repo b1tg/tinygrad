7dc9e4259ab8510866af2256716e9e4e78653c85
b1tg <b1tg@users.noreply.github.com>
2025-03-09 08:06:35 -0700

    relo

--- tinygrad/runtime/support/compiler_hip.py
+++ tinygrad/runtime/support/compiler_hip.py
@@ -1,6 +1,7 @@
 import ctypes, subprocess
 import tinygrad.runtime.autogen.comgr as comgr
 from tinygrad.device import Compiler, CompileError
+# from tinygrad.helpers import getenv
 
 def check(status):
   if status != 0:
@@ -47,10 +48,13 @@ def compile_hip(prg:str, arch="gfx1100", asm=False) -> bytes:
       raise RuntimeError("compile failed")
     check(comgr.amd_comgr_action_info_set_options(action_info, b"-O3 -mllvm -amdgpu-internalize-symbols"))
     check(comgr.amd_comgr_do_action(comgr.AMD_COMGR_ACTION_CODEGEN_BC_TO_RELOCATABLE, action_info, data_set_bc, data_set_reloc))
-
-  check(comgr.amd_comgr_action_info_set_options(action_info, b""))
-  check(comgr.amd_comgr_do_action(comgr.AMD_COMGR_ACTION_LINK_RELOCATABLE_TO_EXECUTABLE, action_info, data_set_reloc, data_set_exec))
-  ret = _get_comgr_data(data_set_exec, comgr.AMD_COMGR_DATA_KIND_EXECUTABLE)
+  ret = _get_comgr_data(data_set_reloc, comgr.AMD_COMGR_DATA_KIND_RELOCATABLE)
+  # if getenv("RELO"):
+  #   ret = _get_comgr_data(data_set_reloc, comgr.AMD_COMGR_DATA_KIND_RELOCATABLE)
+  # else:
+  #   check(comgr.amd_comgr_action_info_set_options(action_info, b""))
+  #   check(comgr.amd_comgr_do_action(comgr.AMD_COMGR_ACTION_LINK_RELOCATABLE_TO_EXECUTABLE, action_info, data_set_reloc, data_set_exec))
+  #   ret = _get_comgr_data(data_set_exec, comgr.AMD_COMGR_DATA_KIND_EXECUTABLE)
   check(comgr.amd_comgr_release_data(data_src))
   for x in [data_set_src, data_set_bc, data_set_reloc, data_set_exec]: check(comgr.amd_comgr_destroy_data_set(x))
   check(comgr.amd_comgr_destroy_action_info(action_info))
