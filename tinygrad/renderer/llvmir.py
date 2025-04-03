45b31f25f1ee85c6333f3329f63470deae56d2ff
b1tg <b1tg@users.noreply.github.com>
2025-04-03 03:45:25 -0700

    exp2

--- tinygrad/renderer/llvmir.py
+++ tinygrad/renderer/llvmir.py
@@ -126,6 +126,7 @@ class LLVMRenderer(Renderer):
   extra_matcher = PatternMatcher([
     # rewrite RECIP with FDIV
     (UPat(Ops.RECIP, name="x"), lambda x: UOp(Ops.FDIV, x.dtype, (x.const_like(1), x.src[0]))),
+    (UPat(Ops.NEG, name="x"), lambda x: UOp(Ops.SUB, x.dtype, (x.const_like(0), x.src[0]))),
     # rewrite cast to bool to CMPNE 0
     (UPat(Ops.CAST, dtype=dtypes.bool, name="x"), lambda x: x.src[0] != x.src[0].const_like(0)),
     # rewrite MAX to CMPLT + WHERE
@@ -139,9 +140,10 @@ class LLVMRenderer(Renderer):
     (UPat(Ops.CAST, name="x", src=UPat.var("y", dtypes.bfloat16)),lambda x,y: y.cast(dtypes.float).cast(x.dtype) if x.dtype!=dtypes.float else None),
     (UPat(Ops.CAST, dtypes.bfloat16, UPat.var("x")),lambda x: x.cast(dtypes.float).cast(dtypes.bfloat16) if x.dtype!=dtypes.float else None),
   ])
-
+  def __getitem__(self, key): return self.r[key]  # hacky helper
   def render(self, uops: list[UOp]) -> str:
     r: dict[UOp, str] = {}
+    self.r = r
     args: list[str] = []
     kernel: list[str] = []
     end_lines: dict[str, None] = {}
@@ -186,7 +188,7 @@ class LLVMRenderer(Renderer):
           r[u] = f"%v{vc}"
 
         # do the rendering of the llvm ir code
-        if (l:=self.string_rewrite.rewrite(u, ctx=r)) is None:
+        if (l:=self.string_rewrite.rewrite(u, ctx=self)) is None:
           raise RuntimeError(f"failed to render {u.op} with {u.dtype} srcs {[x.dtype for x in u.src]}")
         kernel.append(cast(str, l))
 
@@ -212,6 +214,7 @@ attributes #0 = {{ nounwind "no-builtins" "no-trapping-math"="true" }}
 barrier = 'fence syncscope("workgroup") release\ntail call void @llvm.amdgcn.s.barrier()\nfence syncscope("workgroup") acquire\n'
 code_for_workitem = {"g": lambda x: f"tail call i32 @llvm.amdgcn.workgroup.id.{chr(120+int(x))}()",
                      "l": lambda x: f"tail call i32 @llvm.amdgcn.workitem.id.{chr(120+int(x))}()"}
+tp_map, sz_map = {dtypes.half:'half', dtypes.float:'float'}, {dtypes.half:'f16', dtypes.float:'f32'}
 class AMDLLVMRenderer(LLVMRenderer):
   device = "AMD"
   has_local = True
@@ -219,8 +222,15 @@ class AMDLLVMRenderer(LLVMRenderer):
   shared_max = AMDRenderer.shared_max
   global_max = AMDRenderer.global_max
   tensor_cores = AMDRenderer.tensor_cores
+  code_for_op = {
+    Ops.EXP2: lambda x,dtype: f" { tp_map.get(dtype)} @llvm.amdgcn.exp2.{ sz_map.get(dtype)}({ tp_map.get(dtype)} {x});",
+    Ops.LOG2: lambda x,dtype: f" { tp_map.get(dtype)} @llvm.amdgcn.log.{ sz_map.get(dtype)}({ tp_map.get(dtype)} {x});",
+    Ops.SIN: lambda x,dtype: f" { tp_map.get(dtype)} @llvm.amdgcn.sin.{ sz_map.get(dtype)}({ tp_map.get(dtype)} {x});",
+    Ops.SQRT: lambda x,dtype:  f" { tp_map.get(dtype)} @llvm.amdgcn.sqrt.{ sz_map.get(dtype)}({ tp_map.get(dtype)} {x});",
+  }
   abi = "amdgpu_kernel"
   string_rewrite = PatternMatcher([
+    (UPat(GroupOp.Unary, name="x"), lambda ctx,x: f"  {ctx[x]} = call " + ctx.code_for_op[x.op](*([ctx[v] for v in x.src]), x.dtype)),
     (UPat(Ops.SPECIAL, name="x"), lambda ctx, x: f"  {ctx[x]} = " + f"{ code_for_workitem[x.arg[0][0]](x.arg[0][-1])}; "),
     (UPat(Ops.BARRIER), lambda ctx: barrier),
     (UPat(Ops.CAST, name="x", dtype=dtypes.half.vec(16), src=UPat.var("y", dtypes.half.vec(8))), lambda ctx, x, y: f"  {ctx[x]} = shufflevector "\
