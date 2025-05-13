6ba05006fa03780f393fccc86d17d2ead57d5157
b1tg <b1tg@users.noreply.github.com>
2025-05-12 19:55:08 -0700

    test diag error

--- tinygrad/runtime/ops_llvm.py
+++ tinygrad/runtime/ops_llvm.py
@@ -57,7 +57,7 @@ class LLVMCompiler(Compiler):
     llvm.LLVMDisposeModule(mod)
     obj = ctypes.string_at(llvm.LLVMGetBufferStart(obj_buf), llvm.LLVMGetBufferSize(obj_buf))
     llvm.LLVMDisposeMemoryBuffer(obj_buf)
-    if self.diag_msgs: raise RuntimeError("llvm diagnostic:" + "\n".join(self.diag_msgs))
+    if self.diag_msgs: raise RuntimeError("llvm diagnostic: " + "\n".join(self.diag_msgs))
     return jit_loader(obj) if self.jit else obj
 
   def disassemble(self, lib:bytes): capstone_flatdump(lib)
