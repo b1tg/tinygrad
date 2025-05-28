325f0e5826d74241f7d6cd72ca33dfd642276114
b1tg <b1tg@users.noreply.github.com>
2025-05-28 13:52:06 +0000

    old onnx

--- bench1.py
+++ bench1.py
@@ -6,22 +6,20 @@ from extra.onnx_old import OnnxRunner as OnnxRunnerOLD
 import pathlib
 import time
 import onnx
-N=10
+from tinygrad.helpers import getenv
+N=getenv("N", 10)
 import sys
 m = "./model.onnx"
 if len(sys.argv) == 2:
    m = sys.argv[1]
 fm = Tensor(pathlib.Path(m)).to(Device.DEFAULT)
-print(f"to device ok {Device.DEFAULT}")
+print(f"to device {Device.DEFAULT}")
 sts = []
 for i in range(N):
   print(f"---- {i} ----")
   st = time.perf_counter()
   x = onnx_load(fm)
-  print(f"\t{i}: onnx_load use {(time.perf_counter()-st)*1000:6.2f} ms")
-  print("--- run")
   run_onnx = OnnxRunner(x)
-  print(f"\t{i}: run use {(time.perf_counter()-st)*1000:6.2f} ms")
   sts.append(time.perf_counter()-st)
 print(f"onnx_load use {(sum(sts)/N)*1000:6.2f} ms")
 
