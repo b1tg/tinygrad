2ed2e95cd6180cf91bd836038b01c4a37eee2d7c
b1tg <b1tg@users.noreply.github.com>
2025-08-23 23:36:45 +0800

    CUDADevice

--- tinygrad/runtime/ops_nv.py
+++ tinygrad/runtime/ops_nv.py
@@ -524,6 +524,7 @@ class NVDevice(HCQCompiled[HCQSignal]):
     # FIXME: no idea how to convert this for blackwells
     self.arch: str = "sm_120" if self.sm_version==0xa04 else f"sm_{(self.sm_version>>8)&0xff}{(val>>4) if (val:=self.sm_version&0xff) > 0xf else val}"
     self.sass_version = ((self.sm_version & 0xf00) >> 4) | (self.sm_version & 0xf)
+    print(f"== NVDevice: {self.arch=}, {self.sm_version=}, {self.sass_version=}")
 
     compiler_t = (PTXCompiler if PTX else CUDACompiler) if MOCKGPU else (NVPTXCompiler if PTX else NVCompiler)
     super().__init__(device, NVAllocator(self), PTXRenderer(self.arch, device="NV") if PTX else NVRenderer(self.arch), compiler_t(self.arch),
